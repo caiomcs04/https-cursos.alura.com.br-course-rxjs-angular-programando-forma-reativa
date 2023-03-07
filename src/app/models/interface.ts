d27c232ee0f822e50ad17a2d3d1cdc74f0c625cd
caiomcs04 <caio6c@yahoo.com.br>
2023-03-07 17:44:48 -0300

    Ayla 2

---
+++ src/app/models/interface.ts
@@ -0,0 +1,51 @@
+
+export interface VolumeInfo {
+  title: string,
+  authors: Array<string>,
+  publisher:string,
+   publishedDate:string,
+    description: string,
+  pageCount: number,
+  printType:string,
+   mainCategory: string,
+  categories: Array<string>,
+  averageRating: number,
+  ratingsCount: number,
+  contentVersion: string,
+  imageLinks: ImageLinks,
+  language: string,
+  infoLink: string,
+  canonicalVolumeLink: string,
+}
+
+export interface ImageLinks {
+   smallThumbnail: string,
+   thumbnail: string,
+   small: string,
+   medium: string,
+   large:string,
+    extraLarge: string,
+}
+
+export interface Item {
+  volumeInfor: VolumeInfo
+}
+
+export interface LivrosResultado{
+  items: Array<Item>,
+  totalItems: number
+}
+
+export interface Livro{
+  title?: string,
+  authors?: Array<string>,
+  publisher?:string,
+   publishedDate?:string,
+    description?: string,
+    previewLink?: string,
+    thumbnail?: string
+}
+
+
+
+
