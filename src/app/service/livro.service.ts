1bec4d644f62def9ca200974c1b0e8b3ba5c4a36
caiomcs04 <caio6c@yahoo.com.br>
2023-03-07 17:44:28 -0300

    Aula 2

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,3 +1,4 @@
+import {  LivrosResultado } from './../models/interface';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -10,7 +11,7 @@ export class LivroService {
   private readonly API = 'https://www.googleapis.com/books/v1/volumes'
   constructor(private http:HttpClient) { }
 
-  search(value:string): Observable<object>{
+  search(value:string): Observable<LivrosResultado>{
     const params = new HttpParams().append('q', value)
     return this.http.get(this.API, {params})
   }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,30 +1,34 @@
 import { LivroService } from './../../service/livro.service';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subject, Subscription, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
   listaLivros: [];
   searchField: string = '';
+  private _unsubscribe: Subject<boolean> =new Subject;
 
   constructor(private service: LivroService) { }
 
   searchBooks = (): void =>{
-    this.service.search(this.searchField).subscribe(
+    this.service.search(this.searchField).pipe(takeUntil(this._unsubscribe)).subscribe(
       {
         next: (response) => console.log(response),
-        error: (err) => console.warn(err)
+        error: (err) => console.warn(err),
+        complete: () => console.log('Observable completado')
       }
 
       )
     return;
   }
 
+  ngOnDestroy(){
+    this._unsubscribe.next(true)
+    this._unsubscribe.complete()
+  }
 }
-
-
-
