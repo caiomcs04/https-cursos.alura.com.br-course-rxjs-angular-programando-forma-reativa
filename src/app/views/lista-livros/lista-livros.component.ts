039a3a76675a8b8d310090d47975830457fa92ea
caiomcs04 <caio6c@yahoo.com.br>
2023-03-07 22:16:43 -0300

    Aula 05

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,44 +1,31 @@
 import { LivroVolumeInfo } from './../../models/livroVolumeInfo';
-import { Livro, VolumeInfo, Item } from './../../models/interface';
+import { Livro, Item } from './../../models/interface';
 import { LivroService } from './../../service/livro.service';
 import { Component, OnDestroy } from '@angular/core';
-import { Subject, Subscription, takeUntil } from 'rxjs';
+import { debounceTime, filter, map, Subject, switchMap, takeUntil } from 'rxjs';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
-  styleUrls: ['./lista-livros.component.css']
+  styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent implements OnDestroy {
-
+export class ListaLivrosComponent {
   listaLivros: Array<Livro>;
-  searchField: string = '';
-  private _unsubscribe: Subject<boolean> =new Subject;
-  livro : Livro
+  searchField = new FormControl();
+  pauseTime = 300;
+  constructor(private service: LivroService) {}
 
-  constructor(private service: LivroService) { }
+  livrosEncontrados$ = this.searchField.valueChanges.pipe(
+    debounceTime(this.pauseTime),
+    filter((value) => value.length >= 3),
+    switchMap((value) => this.service.search(value)),
+    map((items) => this.resultadosLivros(items))
+  );
 
-  searchBooks = (): void =>{
-    this.service.search(this.searchField).pipe(takeUntil(this._unsubscribe)).subscribe(
-      {
-        next: (items) => {
-          this.listaLivros = this.resultadosLivros(items)
-        },
-        error: (err) => console.warn(err),
-      }
-
-      )
-    return;
-  }
-
-resultadosLivros(items: Array<Item>): Array<LivroVolumeInfo>{
-return items.map(item =>{
-  return new LivroVolumeInfo(item)
-})
-}
-
-  ngOnDestroy(){
-    this._unsubscribe.next(true)
-    this._unsubscribe.complete()
+  resultadosLivros(items: Array<Item>): Array<LivroVolumeInfo> {
+    return items.map((item) => {
+      return new LivroVolumeInfo(item);
+    });
   }
 }
